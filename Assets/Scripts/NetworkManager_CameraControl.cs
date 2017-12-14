c510226bcfc1c11de4ad555bf9be6119287c800b
Spefire <nicholasb@hotmail.fr>
2017-12-14 22:09:37 +0100

    Feat : Add a rotation for the camera lobby

---
+++ Assets/Scripts/NetworkManager_CameraControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManager_CameraControl : NetworkManager {
+
+	[Header("Scene Camera Properties")]
+	public Transform sceneCamera;
+	public float cameraRotationRadius = 24f;
+	public float cameraRotationSpeed = 3f;
+	public bool canRotate = true;
+	private float rotation;
+
+	public override void OnStartClient(NetworkClient client) {
+		canRotate = false;
+	}
+
+	public override void OnStartHost() {
+		canRotate = false;
+	}
+
+	public override void OnStopClient() {
+		canRotate = true;
+	}
+
+	public override void OnStopHost() {
+		canRotate = true;
+	}
+
+	void Update () {
+		if (!canRotate) {
+			return;
+		}
+		rotation += cameraRotationSpeed * Time.deltaTime;
+		if (rotation >= 360f) {
+			rotation -= 360f;
+		}
+		sceneCamera.position = Vector3.zero;
+		sceneCamera.rotation = Quaternion.Euler(0f, rotation, 0f);
+		sceneCamera.Translate (0f, cameraRotationRadius, -cameraRotationRadius);
+		sceneCamera.LookAt (Vector3.zero);
+	}
+}
