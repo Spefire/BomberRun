479a89626762c6c525dd236d28e7a376d8333690
Nicholas Brun <nicholasb@hotmail.fr>
2017-12-18 14:01:12 +0100

    Feat : Add credits and quit

--- Assets/Scripts/NetworkManagerExtension.cs
+++ Assets/Scripts/NetworkManagerExtension.cs
@@ -4,7 +4,9 @@ using UnityEngine.Networking;
 public class NetworkManagerExtension : NetworkManager {
 
 	[Header("Canvas Properties")]
-	public GameObject canvas;
+	public GameObject menuPrincipal;
+	public GameObject menuCredits;
+	private bool isOnCredits;
 	[Header("Scene Camera Properties")]
 	public Transform sceneCamera;
 	public float cameraRotationRadius = 25f;
@@ -15,6 +17,7 @@ public class NetworkManagerExtension : NetworkManager {
 	private MapGeneration genMap;
 
 	void Start () {
+		isOnCredits = false;
 		genMap = GetComponent<MapGeneration> ();
 		genMap.CreateMap ("map01.csv");
 		genMap.CreateStructure ();
@@ -24,28 +27,46 @@ public class NetworkManagerExtension : NetworkManager {
 	public override void OnStartHost() {
 		canRotate = false;
 		genMap.CreateBoxes ();
-		canvas.SetActive (false);
+		menuPrincipal.SetActive (false);
+		menuCredits.SetActive (false);
+		isOnCredits = false;
 	}
 
 	public override void OnStartClient(NetworkClient client) {
 		canRotate = false;
-		canvas.SetActive (false);
+		menuPrincipal.SetActive (false);
+		menuCredits.SetActive (false);
+		isOnCredits = false;
 	}
 
 	public override void OnStopHost() {
 		canRotate = true;
-		canvas.SetActive (true);
+		menuPrincipal.SetActive (true);
 	}
 
 	public override void OnStopClient() {
 		canRotate = true;
-		canvas.SetActive (true);
+		menuPrincipal.SetActive (true);
 	}
 
 	void Update () {
 		if (!canRotate) {
 			return;
 		}
+
+		if (!isOnCredits && Input.GetKeyDown (KeyCode.Q)) {
+			Application.Quit ();
+		}
+		if (!isOnCredits && Input.GetKeyDown (KeyCode.D)) {
+			menuPrincipal.SetActive (false);
+			menuCredits.SetActive (true);
+			isOnCredits = true;
+		} else if (isOnCredits && Input.GetKeyDown (KeyCode.D)) {
+			menuCredits.SetActive (false);
+			menuPrincipal.SetActive (true);
+			isOnCredits = false;
+		}
+
 		cameraRotation += cameraRotationSpeed * Time.deltaTime;
 		if (cameraRotation >= 360f) {
 			cameraRotation -= 360f;
