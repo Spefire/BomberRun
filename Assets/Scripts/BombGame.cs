afe3fb79326272844e675e974ab00712a8b62ec1
Nicholas Brun <nicholasb@hotmail.fr>
2017-12-18 16:40:32 +0100

    Refractor : Use a different script for bomb life, and resize prefabs

---
+++ Assets/Scripts/BombGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BombGame : NetworkBehaviour {
+
+	public float bombLifetime = 5.0f;
+	private bool isLive = true;
+	private float age;
+
+	//---------------------------------------------------------------------------------------------
+	//---------------------------------------------------------------------------------------------
+
+	void Start () {
+		age = 0.0f;
+	}
+
+	[ServerCallback]
+	void Update () {
+		age += Time.deltaTime;
+		if (age > bombLifetime) {
+			NetworkServer.Destroy (gameObject);
+		}
+	}
+}
