7e10f4ba1759c01c58afd84e52d8ecfa14435805
Spefire <nicholasb@hotmail.fr>
2017-12-18 23:27:15 +0100

    Feat : Use the reload

--- Assets/Scripts/BombGame.cs
+++ Assets/Scripts/BombGame.cs
@@ -8,6 +8,7 @@ public class BombGame : NetworkBehaviour {
 	public float bombLifetime = 5.0f;
 	private bool isLive = true;
 	private float age;
+	private PlayerGame pg;
 
 	//---------------------------------------------------------------------------------------------
 	//---------------------------------------------------------------------------------------------
@@ -20,7 +21,12 @@ public class BombGame : NetworkBehaviour {
 	void Update () {
 		age += Time.deltaTime;
 		if (age > bombLifetime) {
+			pg.numberBombs++;
 			NetworkServer.Destroy (gameObject);
 		}
+	}
+
+	public void SetPlayerGame(PlayerGame pg) {
+		this.pg = pg;
 	}
 }
