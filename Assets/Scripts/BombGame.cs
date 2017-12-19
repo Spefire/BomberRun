f0e0220ad0b2760f5ef7de09bf10c232e67beeee
Nicholas Brun <nicholasb@hotmail.fr>
2017-12-19 13:48:19 +0100

    Feat : Add ray for the explosion

--- Assets/Scripts/BombGame.cs
+++ Assets/Scripts/BombGame.cs
@@ -5,8 +5,9 @@ using UnityEngine.Networking;
 
 public class BombGame : NetworkBehaviour {
 
+	public GameObject rayPrefab;
 	public float bombLifetime = 5.0f;
-	private bool isLive = true;
+	public int powerLevel = 2;
 	private float age;
 	private PlayerGame pg;
 
@@ -22,6 +23,26 @@ public class BombGame : NetworkBehaviour {
 		age += Time.deltaTime;
 		if (age > bombLifetime) {
 			pg.numberBombs++;
+			//1
+			Vector3 pos = new Vector3(this.transform.position.x-0.5f, this.transform.position.y, this.transform.position.z);
+			GameObject g = (GameObject) Instantiate (rayPrefab, pos, this.transform.rotation);
+			NetworkServer.Spawn (g);
+			//2
+			this.transform.Rotate (new Vector3 (0, 90, 0));
+			pos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z+0.5f);
+			g = (GameObject) Instantiate (rayPrefab, pos, this.transform.rotation);
+			NetworkServer.Spawn (g);
+			//3
+			this.transform.Rotate (new Vector3 (0, 90, 0));
+			pos = new Vector3(this.transform.position.x+0.5f, this.transform.position.y, this.transform.position.z);
+			g = (GameObject) Instantiate (rayPrefab, pos, this.transform.rotation);
+			NetworkServer.Spawn (g);
+			//4
+			this.transform.Rotate (new Vector3 (0, 90, 0));
+			pos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z-0.5f);
+			g = (GameObject) Instantiate (rayPrefab, pos, this.transform.rotation);
+			NetworkServer.Spawn (g);
+			//
 			NetworkServer.Destroy (gameObject);
 		}
 	}
