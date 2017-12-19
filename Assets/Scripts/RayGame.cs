69ce83495a9bcdab1ca0844187f644ca4165543c
Nicholas Brun <nicholasb@hotmail.fr>
2017-12-19 14:18:05 +0100

    Feat : Use rays and their scales

---
+++ Assets/Scripts/RayGame.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RayGame : NetworkBehaviour {
+
+	public float speed = 0.1f;
+	private int orientation;
+	private float currentSize;
+	private bool canExpand;
+	private BombGame bg;
+
+	//---------------------------------------------------------------------------------------------
+	//---------------------------------------------------------------------------------------------
+
+	void Start() {
+		currentSize = 0.0f;
+	}
+
+	[ServerCallback]
+	void Update () {
+		if (canExpand) {
+			if (currentSize < (bg.powerLevel - 1)) {
+				currentSize += speed * Time.deltaTime;
+				this.transform.localScale += new Vector3 (speed * Time.deltaTime, 0, 0);
+				switch (orientation) {
+				case 1:
+					this.transform.position += new Vector3 (-speed * Time.deltaTime / 2, 0, 0);
+					break;
+				case 2:
+					this.transform.position += new Vector3 (0, 0, speed * Time.deltaTime / 2);
+					break;
+				case 3:
+					this.transform.position += new Vector3 (speed * Time.deltaTime / 2, 0, 0);
+					break;
+				case 4:
+					this.transform.position += new Vector3 (0, 0, -speed * Time.deltaTime / 2);
+					break;
+				}
+			} else {
+				NetworkServer.Destroy (this.gameObject);
+			}
+		}
+	}
+
+	public void SetBombGame(BombGame bg, int orientation) {
+		this.bg = bg;
+		this.canExpand = true;
+		this.orientation = orientation;
+	}
+}
